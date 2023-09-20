4febd5535d5fa5a200c3edc2f5fb7b0ba48cb0bb
ChristinPrabuSC <145604426+ChristinPrabuSC@users.noreply.github.com>
2023-09-20 20:31:06 +0200

    Add files via upload
    week1day1

---
+++ day1/LearnDataType.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class LearnDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//variable -> lowercase (camelcase)
+		//datatype -> varName value/data
+		
+		String name ="Christin Prabu";
+		short age = 25;
+		int accountNum = 8698689;
+		long phonenumber = 9922988401L;
+		double acctBalance = 500.16;
+		float rateOfInterest = 6.5f;
+		boolean isActive = true;
+		char logo = 'R';
+		System.out.println("My name is " + name);
+		System.out.println(accountNum+" "+isActive);
+
+	}
+
+}
